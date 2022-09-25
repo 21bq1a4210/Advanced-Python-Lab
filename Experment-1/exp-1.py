9700a1df75bfb5ce1385cc9099e2064af614488d
21bq1a4210 <113082987+21bq1a4210@users.noreply.github.com>
2022-09-25 21:22:08 +0530

    Update exp-1.py
    added comments

--- Experment-1/exp-1.py
+++ Experment-1/exp-1.py
@@ -1,31 +1,22 @@
-class Check: 
+class Check:# class
+    def __init__(self,s): #constructor
+        self.sen=s; #input string
+        self.vowels={'a','e','i','o','u'}; #set of ovwels
+        self.ch=[]; #list for storeing each and every char of input str
 
-    def __init__(self,s): 
-
-        self.sen=s; 
-
-        self.vowels={'a','e','i','o','u'}; 
-
-        self.ch=[]; 
-
-    def check(self): 
-
-        self.sen=self.sen.lower() 
-
+    def check(self): #logic
+        self.sen=self.sen.lower() #making input into lower
+        
         for ch in self.sen: 
-
-            self.ch.append(ch) 
-
-        self.ch=set(self.ch) 
-
-        if len(self.ch & self.vowels)==len(self.vowels): 
-
+            self.ch.append(ch) #storeing each and every char in ch list
+            
+        self.ch=set(self.ch) #converting ch list into set
+        
+        if len(self.ch & self.vowels)==len(self.vowels): #checking for condition 
             print("sentence contain all the vowels") 
-
+            
         else: 
-
             print(" sentence don’t contain all the vowels ") 
 
-obj=Check(input("enter a sentence:")) 
-
-obj.check() 
+obj=Check(input("enter a sentence:")) #creating obj for class Check
+obj.check() #invoking the method
